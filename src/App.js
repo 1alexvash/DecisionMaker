b38f98b83c2a7fc1b107c414423fa1f3d19de174
Alexander <1alexvash@gmail.com>
2020-12-11 08:28:29 +0200

    Added debug function

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import { StoreProvider, useStoreState } from "easy-peasy";
+import { StoreProvider, useStoreState, useStoreActions } from "easy-peasy";
 
 import HomeScreen from "./screens/HomeScreen";
 
@@ -8,10 +8,12 @@ import store from "./store";
 
 const App = () => {
   const { hello } = useStoreState((state) => state);
+  const { sayHello } = useStoreActions((actions) => actions);
 
   return (
     <div className="App">
       {hello}
+      <button onClick={() => sayHello()}>Push Me</button>
       <Router>
         <Route path="/" component={HomeScreen}></Route>
       </Router>
--- src/store.js
+++ src/store.js
@@ -1,6 +1,10 @@
 import { createStore } from "easy-peasy";
 import actions from "./actions";
 
+const debug = (data) => JSON.parse(JSON.stringify(data));
+
+console.log("Hello");
+
 const store = createStore({
   hello: "hello",
 
