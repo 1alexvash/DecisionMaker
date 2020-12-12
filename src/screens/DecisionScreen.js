26d7d2e39f28c00d604e88b1d93b57a75770663c
Alexander <1alexvash@gmail.com>
2020-12-12 18:24:38 +0200

    Decision page catches the right decision

--- src/screens/DecisionScreen.js
+++ src/screens/DecisionScreen.js
@@ -1,10 +1,15 @@
 import React, { useEffect } from "react";
+import { useStoreState } from "easy-peasy";
 
 const DecisionScreen = ({ match }) => {
+  const { decisions } = useStoreState((state) => state);
+
   useEffect(() => {
     const id = match.params.id;
-    console.log("match is ready", id);
-  }, [match]);
+
+    const decision = decisions.find((decision) => decision.url === id);
+    console.log("decision:", decision);
+  }, [match, decisions]);
 
   return <div>Let's display a decision</div>;
 };
