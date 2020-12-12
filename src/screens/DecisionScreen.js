09d4b62c6aa5851a3c34384442f65b043a132ed0
Alexander <1alexvash@gmail.com>
2020-12-12 22:02:17 +0200

    Added scrolls

--- src/screens/DecisionScreen.js
+++ src/screens/DecisionScreen.js
@@ -14,8 +14,42 @@ const DecisionScreen = ({ match }) => {
     return <Redirect to="/" />;
   }
 
+  const factors = [
+    {
+      name: "Weather",
+      importance: 0,
+      firstChoice: 0,
+      secondChoice: 0,
+    },
+  ];
+
   return (
     <div className="Decision">
+      <div className="factors">
+        {factors.map((factor) => (
+          <div
+            className="factor"
+            style={{
+              background: "#eee",
+              padding: "20px",
+              margin: "20px",
+              border: "2px solid #333",
+            }}
+          >
+            <p>
+              {factor.name}: {factor.importance}
+            </p>
+            <input type="range" min="0" max="10" title="Importance" />
+
+            <p>Dalas [3]</p>
+            <input type="range" min="0" max="10" />
+
+            <p>Detrois [2]</p>
+            <input type="range" min="0" max="10" />
+          </div>
+        ))}
+      </div>
+      ### total scorll at the bottom
       <form className="new-factor">
         <input type="text" placeholder="New Factor" />
         <button type="button">Add New Factor</button>
