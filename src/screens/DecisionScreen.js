69bad1807449055bd03b6c971a083d981fb1f8c1
Alexander <1alexvash@gmail.com>
2020-12-12 18:27:49 +0200

    redirecting in case of unexisting page

--- src/screens/DecisionScreen.js
+++ src/screens/DecisionScreen.js
@@ -1,14 +1,21 @@
 import React, { useEffect } from "react";
+
+import { useHistory } from "react-router-dom";
+
 import { useStoreState } from "easy-peasy";
 
 const DecisionScreen = ({ match }) => {
   const { decisions } = useStoreState((state) => state);
+  let history = useHistory();
 
   useEffect(() => {
     const id = match.params.id;
 
     const decision = decisions.find((decision) => decision.url === id);
-    console.log("decision:", decision);
+
+    if (!decision) {
+      history.push("/");
+    }
   }, [match, decisions]);
 
   return <div>Let's display a decision</div>;
